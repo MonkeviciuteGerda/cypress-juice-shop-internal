0722e463f41be51491f827c15965c0126179300d
GerdaMonkeviciute <gerda.monkeviciute@devbridge.com>
2020-06-01 16:10:49 +0300

    Fix POM

--- cypress/page-objects/address/assertAddressPage.js
+++ cypress/page-objects/address/assertAddressPage.js
@@ -1,12 +1,30 @@
-import AssertAddressForm from './assertAddressForm';
-
 class AssertAddressPage {
-    get assertAddressForm() {
-        return new AssertAddressForm();
+    getLastRowNameColumn() { 
+        return cy.get('[data-cy="addressTableRow"]').last().get('[data-cy="addressName"]'); 
     }
 
+    getLastRowAddressColumn() { 
+        return cy.get('[data-cy="addressTableRow"]').last().get('[data-cy="fullAddress"]'); 
+    }
+
+    getLastRowCountryColumn() { 
+        return cy.get('[data-cy="addressTableRow"]').last().get('[data-cy="country"]'); 
+    }
+
+    getLastRowEditColumn() { 
+        return cy.get('[data-cy="addressTableRow"]').last().get('[data-cy="editAddressButton"]'); 
+    }
+    
+    getLastRowDeleteColumn() { 
+        return cy.get('[data-cy="addressTableRow"]').last().get('[data-cy="deleteAddressButton"]'); 
+    }
+ 
     assertAddressTableLastRow(details) {
-        this.assertAddressForm.assertAddressTableLastRow(details);
+        this.getLastRowNameColumn().should('contain.text', details.name);
+        this.getLastRowAddressColumn().should('contain.text', details.address);
+        this.getLastRowCountryColumn().should('contain.text', details.country);
+        this.getLastRowEditColumn().should('be.visible');
+        this.getLastRowDeleteColumn().should('be.visible');
     }
 }
 
